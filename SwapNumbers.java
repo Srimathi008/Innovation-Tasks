71568a6eccc659a5b4cfd98e71b5e457bb855185
Srimathi008 <srimathimageswaran@gmail.com>
2025-01-31 15:14:11 +0530

    Add files via upload

---
+++ SwapNumbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+
+      
+        System.out.println("After swapping: " + num1 + " " + num2);
+        
+        sc.close();
+    }
+}
