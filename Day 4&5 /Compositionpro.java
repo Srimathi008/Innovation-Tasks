f489220117669ad2170b223d17119bb0f6190974
Srimathi008 <srimathimageswaran@gmail.com>
2025-03-11 15:30:37 +0530

    composition completed

---
+++ Day 4&5 /Compositionpro.java
@@ -0,0 +1,33 @@
+class Battery {
+    int capacity;
+
+    public Battery(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void showCapacity() {
+        System.out.println("Battery capacity: " + capacity + "mAh");
+    }
+}
+
+class Mobile {
+    String model;
+    Battery battery;
+
+    public Mobile(String model, int batteryCapacity) {
+        this.model = model;
+        this.battery = new Battery(batteryCapacity);
+    }
+
+    public void showInfo() {
+        System.out.println("Mobile model: " + model);
+        battery.showCapacity();
+    }
+}
+
+public class Compositionpro {
+    public static void main(String[] args) {
+        Mobile mobile = new Mobile("Samsung Galaxy", 5000);
+        mobile.showInfo();
+    }
+}
